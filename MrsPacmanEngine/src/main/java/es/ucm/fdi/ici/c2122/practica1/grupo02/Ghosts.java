b4da24c851650cf9851bdf5026757d1565a8436b
mihern07 <mihern07@ucm.es>
2021-10-01 19:31:05 +0200

    Lurker y agressive integrados

--- MrsPacmanEngine/src/main/java/es/ucm/fdi/ici/c2122/practica1/grupo02/Ghosts.java
+++ MrsPacmanEngine/src/main/java/es/ucm/fdi/ici/c2122/practica1/grupo02/Ghosts.java
@@ -1,10 +1,12 @@
 package es.ucm.fdi.ici.c2122.practica1.grupo02;
 
+import java.awt.Color;
 import java.util.EnumMap;
 import java.util.Random;
 
 import pacman.controllers.GhostController;
 import pacman.game.Game;
+import pacman.game.GameView;
 import pacman.game.Constants.DM;
 import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
@@ -13,6 +15,12 @@ public class Ghosts extends GhostController {
 	private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);
 	private MOVE[] allMoves = MOVE.values();
 	private Random rnd = new Random();
+	private int chaseDistance = 35;
+	private int chaseTime = 2;
+	private int [] countGhost = {0, 0, 0, 0};
+	private int awayFromPacmanDistance = 40;
+	private int minPredictionDistance = 15;
+
 
 	@Override
 	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
@@ -20,6 +28,22 @@ public class Ghosts extends GhostController {
 		
 		int pacmanNode = game.getPacmanCurrentNodeIndex();
 
+		
+		//We get the next possible destination of the pacman
+		int [] activePills = game.getActivePillsIndices();
+		int nearestPillNode = -1;
+		int shortestDistance = -1;
+		
+		for (int activePill : activePills) {
+			int distance = game.getShortestPathDistance(pacmanNode, activePill,
+					game.getPacmanLastMoveMade());
+			if(shortestDistance == -1 || game.getShortestPathDistance(pacmanNode, activePill,
+					game.getPacmanLastMoveMade()) < shortestDistance && distance > minPredictionDistance) {
+				nearestPillNode = activePill;
+				shortestDistance = distance;
+			}
+		}
+		
 		for (GHOST ghostType : GHOST.values()) {
 			if (!game.doesGhostRequireAction(ghostType)) {
 				moves.put(ghostType, MOVE.NEUTRAL);		
@@ -27,19 +51,31 @@ public class Ghosts extends GhostController {
 			else {
 				int ghostNode = game.getGhostCurrentNodeIndex(ghostType);
 				
+				
 				if (isGhostEdible(game, ghostType) || pacmanCloseToPill(game, pacmanNode, 25)) {
 					moves.put(ghostType, game.getApproximateNextMoveAwayFromTarget(ghostNode, pacmanNode,
 							game.getGhostLastMoveMade(ghostType), DM.PATH));
-//					System.out.print("RUN\n");
-				}
-				else if (rnd.nextInt(100) < 90) {
-					moves.put(ghostType, game.getApproximateNextMoveTowardsTarget(ghostNode, pacmanNode,
-							game.getGhostLastMoveMade(ghostType), DM.PATH));
-//					System.out.print("CHASE\n");
 				}
 				else {
-					moves.put(ghostType, allMoves[rnd.nextInt(allMoves.length)]);
-//					System.out.print("RAND\n");
+					int ghostDistance;
+					ghostDistance = game.getShortestPathDistance(game.getPacmanCurrentNodeIndex(), ghostNode);
+					if (ghostDistance > awayFromPacmanDistance && game.getShortestPathDistance(ghostNode, nearestPillNode) > 1) {
+						//Go to next pill
+						moves.put(ghostType, game.getApproximateNextMoveTowardsTarget(ghostNode, nearestPillNode,
+							game.getGhostLastMoveMade(ghostType), DM.PATH));
+							
+						GameView.addLines(game,Color.RED, ghostNode, nearestPillNode);
+							
+					} else {
+						//Chase Pacman
+						nextMoveAgressive(game, ghostNode, pacmanNode, ghostType, ghostType.ordinal());
+					}
+					
+					//System.out.print("DECISION\n");
+					//System.out.print(countGhost[0] + "\n");
+					//System.out.print(countGhost[1] + "\n");
+					//System.out.print(countGhost[2] + "\n");
+					//System.out.print(countGhost[3] + "\n");
 				}
 			}
 		}
@@ -59,6 +95,57 @@ public class Ghosts extends GhostController {
 		}
 
 		return false;
+	}
+	
+	private void nextMoveAgressive(Game game, int ghostNode, int pacmanNode, GHOST ghostType, int numGhost) {
+		int ghostDistance;
+		
+		ghostDistance = game.getShortestPathDistance(game.getPacmanCurrentNodeIndex(), ghostNode);
+		if (ghostDistance <= chaseDistance)
+			countGhost[numGhost]++;
+		else countGhost[numGhost] = 0;
+		
+		//Secondary path
+		if (countGhost[numGhost] >= chaseTime) {
+			
+			//System.out.print("Secondary Path\n");
+			
+			moves.put(ghostType, getSecondaryPath(game, game.getPossibleMoves(ghostNode, game.getGhostLastMoveMade(ghostType)),
+					ghostNode, pacmanNode, ghostType));
+			countGhost[numGhost] = 0;
+		//Optimal path
+		} else {
+			//System.out.print("Optimal Path\n");
+			
+			moves.put(ghostType, game.getApproximateNextMoveTowardsTarget(ghostNode, pacmanNode,
+					game.getGhostLastMoveMade(ghostType), DM.PATH));
+		}
+	}
+	
+	
+	private MOVE getSecondaryPath(Game game, MOVE[] possibleMoves, int ghostNode, int pacmanNode, GHOST ghostType) {
+		int numChoices = possibleMoves.length;
+		
+		if (numChoices == 2) {
+			return game.getApproximateNextMoveAwayFromTarget(ghostNode, pacmanNode,
+					game.getGhostLastMoveMade(ghostType), DM.PATH);
+		}
+		else {
+			MOVE optimalMove = game.getApproximateNextMoveTowardsTarget(ghostNode, pacmanNode,
+							   game.getGhostLastMoveMade(ghostType), DM.PATH);
+			
+			MOVE awayMove = game.getApproximateNextMoveAwayFromTarget(ghostNode, pacmanNode,
+						    game.getGhostLastMoveMade(ghostType), DM.PATH);
+			
+			if (possibleMoves[0] != optimalMove && possibleMoves[0] != awayMove) {
+				return possibleMoves[0];
+			} else if (possibleMoves[1] != optimalMove && possibleMoves[1] != awayMove) {
+				return possibleMoves[1];
+			} else {
+				return possibleMoves[2];
+			}
+			
+		}
 	}
 
 	private boolean isGhostEdible(Game game, GHOST g) {
