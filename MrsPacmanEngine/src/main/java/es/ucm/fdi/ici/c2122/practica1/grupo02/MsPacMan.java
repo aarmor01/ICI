40565bf4691e530c85871a48762f0012ed5e741b
JosedaMachine <jrave@ucm.es>
2021-10-03 19:27:43 +0200

    Comentado el codigo de MsPacMan

--- MrsPacmanEngine/src/main/java/es/ucm/fdi/ici/c2122/practica1/grupo02/MsPacMan.java
+++ MrsPacmanEngine/src/main/java/es/ucm/fdi/ici/c2122/practica1/grupo02/MsPacMan.java
@@ -20,6 +20,7 @@ public class MsPacMan extends PacmanController {
 		GHOST nearestGhostType = nearestGhost(game, pacmanNode, chaseDistance);
 		//If there's near ghost
 		if (nearestGhostType != null) {
+			//We color the nodes that represent the path to the ghost
 			GameView.addPoints(game, colours[nearestGhostType.ordinal()],
 					game.getShortestPath(game.getGhostCurrentNodeIndex(nearestGhostType), pacmanNode));
 			//If the ghost is edible, we go for it
@@ -27,16 +28,17 @@ public class MsPacMan extends PacmanController {
 				return game.getApproximateNextMoveTowardsTarget(pacmanNode,
 						game.getGhostCurrentNodeIndex(nearestGhostType), game.getPacmanLastMoveMade(), DM.PATH);
 			else {
+				//Check nearest Power Pill taking into account if the path is blocked or may be blocked by a ghost 
 				MOVE nextMove = getNextPossibleMoveToPowerPill(game, pacmanNode, nearestGhostType);
-				//Si no hay PP o si los caminos hacia ellas estan bloqueados, huye
+				//If there's not PowerPills left, or if the path is block, we ran away
 				if (nextMove == MOVE.NEUTRAL)
 					return game.getApproximateNextMoveAwayFromTarget(pacmanNode,
 							game.getGhostCurrentNodeIndex(nearestGhostType), game.getPacmanLastMoveMade(), DM.PATH);
-				else //De lo contario, se dirige a la PP mas cercana no obstruida
+				else //Otherwise, we go torwards nearests PPill not blocked
 					return nextMove;
 			}
 		}
-		//Si no hay fantasmas cercanos, persigue las pills normales
+		//If there's not near Ghost, we go for active normal pills
 		return moveTowardsPills(game, pacmanNode);
 	}
 	
@@ -46,6 +48,8 @@ public class MsPacMan extends PacmanController {
 
 		for (int activePill : activePowerPills) {
 			int distance = game.getShortestPathDistance(pcNode, activePill, game.getPacmanLastMoveMade());
+			//For every PPill we check which one is the nearest and whose path is not
+			//blocked.
 			if (nearestPP == -1 || game.getShortestPathDistance(pcNode, activePill,
 					game.getPacmanLastMoveMade()) < shortestDist) {
 				if(pathNotBlocked(game, pcNode, activePill, ghostType)) {
@@ -55,6 +59,7 @@ public class MsPacMan extends PacmanController {
 			}
 		}
 		
+		//If there's not PP, we use "move neutral" as a save movement, so we can change and ran away 
 		if (nearestPP == -1)
 			return MOVE.NEUTRAL;
 		else
@@ -67,7 +72,7 @@ public class MsPacMan extends PacmanController {
 
 		for (GHOST ghostType : GHOST.values()) {
 			int ghostIndex = game.getGhostCurrentNodeIndex(ghostType);
-			//Si el fantasma no esta en la jaula
+			//If the ghost is not into jail, we check if it's near to MsPacMan 
 			if (game.getGhostLairTime(ghostType) <= 0) {
 				int distance = game.getShortestPathDistance(pcNode, ghostIndex, game.getPacmanLastMoveMade());
 
@@ -85,7 +90,8 @@ public class MsPacMan extends PacmanController {
 		int[] activePills = game.getActivePillsIndices();
 		int nearestPillNode = -1;
 		int shortestDistance = -1;
-
+		
+		//For each active Pill we check which one is the nearest
 		for (int activePill : activePills) {
 			int distance = game.getShortestPathDistance(pcNode, activePill, game.getPacmanLastMoveMade());
 			if (shortestDistance == -1 || distance < shortestDistance) {
@@ -95,7 +101,7 @@ public class MsPacMan extends PacmanController {
 			}
 		}
 
-		//Si existe Pill cercana, dibujamos su camino euclideo
+		//if there's active pill we draw it's euclidean path
 		if (nearestPillNode != -1)
 			GameView.addLines(game, Color.CYAN, pcNode, nearestPillNode);
 		
@@ -103,13 +109,14 @@ public class MsPacMan extends PacmanController {
 	}
 
 	boolean pathNotBlocked(Game game, int pcNode, int activePillNode, GHOST nearestGhostType_) {
+		//Min distance to predict if the nearest Ghost can block up MsPacMan's path 
 		int obstructionLimit = 40;
-		
+		//We get all the nodes that take MsPacMan to the pill
 		int[] path = game.getShortestPath(pcNode, activePillNode);
 		
 		int ghostIndex = game.getGhostCurrentNodeIndex(nearestGhostType_);
 		
-		
+		//For each Ghost we check if block up the MsPacMan path or may block it up.
 		for(int pathNode : path) {
 			for(GHOST ghostType: GHOST.values()) {
 				if(pathNode == game.getGhostCurrentNodeIndex(ghostType))
