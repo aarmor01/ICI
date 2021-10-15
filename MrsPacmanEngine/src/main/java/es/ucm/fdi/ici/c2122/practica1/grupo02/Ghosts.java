ba6d6200f54a84c2f97b6bcacd0fae53deffcfda
mihern07 <mihern07@ucm.es>
2021-10-15 20:57:15 +0200

    Cambiado el comportamiento del fantasma a estados

--- MrsPacmanEngine/src/main/java/es/ucm/fdi/ici/c2122/practica1/grupo02/Ghosts.java
+++ MrsPacmanEngine/src/main/java/es/ucm/fdi/ici/c2122/practica1/grupo02/Ghosts.java
@@ -2,7 +2,6 @@ package es.ucm.fdi.ici.c2122.practica1.grupo02;
 
 import java.awt.Color;
 import java.util.EnumMap;
-import java.util.Random;
 
 import pacman.controllers.GhostController;
 import pacman.game.Game;
@@ -13,12 +12,16 @@ import pacman.game.Constants.MOVE;
 
 public class Ghosts extends GhostController {
 	private EnumMap<GHOST, MOVE> moves = new EnumMap<GHOST, MOVE>(GHOST.class);
-	private MOVE[] allMoves = MOVE.values();
 
 	// Aggresive behaviour data
 	private int chaseTime = 2; // Number of junctions max before changing ghosts' path
 	private int[] countGhost = { 0, 0, 0, 0 }; // Junctions' counter for every ghost
 
+	private enum StatesSeer{ Away, Chasing, RunningAway };
+	private StatesSeer stateSeer = StatesSeer.Away;
+	
+	boolean DEBUG = true;
+	
 	// Lurker behaviour data
 	private int awayFromPacmanDistance = 40; // Distance that changes to aggresive behaviour
 	private int minPredictionDistance = 15; // Range to check pills that are further than this distance
@@ -27,66 +30,145 @@ public class Ghosts extends GhostController {
 	public EnumMap<GHOST, MOVE> getMove(Game game, long timeDue) {
 		moves.clear();
 		
+		GHOST ghosts[] = GHOST.values();
+		
+		//Seer
+		seerStateMachine(game, ghosts[0]);
+				
+		//Intersect
+		//IN NEED OF DEVELOPMENT
+		nextIntersectGhost(game);
+				
+		//Ant
+		//IN NEED OF DEVELOPMENT
+				
+				
+		//Agressive
+		if (game.doesGhostRequireAction(ghosts[3]))
+			nextMoveAgressive(game, game.getGhostCurrentNodeIndex(ghosts[3]), game.getPacmanCurrentNodeIndex(), ghosts[3], 3);
+		
+		return moves;
+	}
+	
+	boolean ghostNotOnSamePath(Game game, int ghostNode, int destination) {
+		//All the nodes in the path we want to check
+		int[] path = game.getShortestPath(ghostNode, destination);
+		
+		//For each Ghost we check if there is another ghost in the path
+		for(int pathNode : path) {
+			for(GHOST ghostType: GHOST.values()) {
+				int ghostInPath = game.getGhostCurrentNodeIndex(ghostType);
+				if (ghostInPath != ghostNode && pathNode == ghostInPath)
+					return false;
+			}	
+		}
+		
+		return true;
+	}
+	
+	private void seerStateMachine(Game game, GHOST g) {
 		// We get Pacman current node
 		int pacmanNode = game.getPacmanCurrentNodeIndex();
 		
-		// We get the next possible destination of the Pacman
-		int[] activePills = game.getActivePillsIndices();
-		int nearestPillNode = -1;
-		int shortestDistance = -1;
+		int ghostNode = game.getGhostCurrentNodeIndex(g);
+		
 
-		for (int activePill : activePills) {
-			int distance = game.getShortestPathDistance(pacmanNode, activePill, game.getPacmanLastMoveMade());
-			if (shortestDistance == -1 || distance < shortestDistance && distance > minPredictionDistance) {
-				nearestPillNode = activePill;
-				shortestDistance = distance;
+		if (!game.doesGhostRequireAction(g)) 
+			moves.put(g, MOVE.NEUTRAL);
+		else {
+			int[] activePills = game.getActivePillsIndices();
+			int nearestPillNode = -1;
+			int shortestDistance = -1;
+			
+			// We get the next possible destination of the Pacman
+			for (int activePill : activePills) {
+				int distance = game.getShortestPathDistance(pacmanNode, activePill, game.getPacmanLastMoveMade());
+				if (shortestDistance == -1 || distance < shortestDistance && distance > minPredictionDistance) {
+					nearestPillNode = activePill;
+					shortestDistance = distance;
+				}
 			}
-		}
-
-		for (GHOST ghostType : GHOST.values()) {
-			if (!game.doesGhostRequireAction(ghostType)) 
-				moves.put(ghostType, MOVE.NEUTRAL);
-			else {
-				int ghostNode = game.getGhostCurrentNodeIndex(ghostType);
-
+			
+			switch(stateSeer) {
+			case Away:
 				// If Pacman has been powered up, or it's close to eat a power pill, the ghost runs away from her
-				if (isGhostEdible(game, ghostType) || pacmanCloseToPowerPill(game, pacmanNode, 25))
-					moves.put(ghostType, game.getApproximateNextMoveAwayFromTarget(ghostNode, pacmanNode,
-							game.getGhostLastMoveMade(ghostType), DM.PATH));
+				if (isGhostEdible(game, g) || pacmanCloseToPowerPill(game, pacmanNode, 25)) {
+					moves.put(g, game.getApproximateNextMoveAwayFromTarget(ghostNode, pacmanNode,
+							game.getGhostLastMoveMade(g), DM.PATH));
+					stateSeer = StatesSeer.RunningAway;
+				}
 				else {
 					int ghostDistance; // distance between the ghost and Pacman
 					ghostDistance = game.getShortestPathDistance(pacmanNode, ghostNode);
 					
 					// Go to next pill if it's not next to the Pacman
 					if (ghostDistance > awayFromPacmanDistance && game.getShortestPathDistance(ghostNode, nearestPillNode) > 1) {
-						moves.put(ghostType, game.getApproximateNextMoveTowardsTarget(ghostNode, nearestPillNode,
-								game.getGhostLastMoveMade(ghostType), DM.PATH));
+						moves.put(g, game.getApproximateNextMoveTowardsTarget(ghostNode, nearestPillNode,
+								game.getGhostLastMoveMade(g), DM.PATH));
 
-						GameView.addLines(game, Color.RED, ghostNode, nearestPillNode);
+						if (DEBUG)
+							GameView.addLines(game, Color.RED, ghostNode, nearestPillNode);
 					}
 					else // if the ghost is close enough, it chases Pacman
-						nextMoveAgressive(game, ghostNode, pacmanNode, ghostType, ghostType.ordinal());
+						stateSeer = StatesSeer.Chasing;
+						nextMoveAgressive(game, ghostNode, pacmanNode, g, g.ordinal());
 				}
+				break;
+				
+			case Chasing:
+				if (isGhostEdible(game, g) || pacmanCloseToPowerPill(game, pacmanNode, 25)) {
+					moves.put(g, game.getApproximateNextMoveAwayFromTarget(ghostNode, pacmanNode,
+							game.getGhostLastMoveMade(g), DM.PATH));
+					stateSeer = StatesSeer.RunningAway;
+				}
+				else {
+					int ghostDistance; // distance between the ghost and Pacman
+					ghostDistance = game.getShortestPathDistance(pacmanNode, ghostNode);
+					
+					// Go to next pill if it's not next to the Pacman
+					if (ghostDistance > awayFromPacmanDistance && game.getShortestPathDistance(ghostNode, nearestPillNode) > 1) {
+						moves.put(g, game.getApproximateNextMoveTowardsTarget(ghostNode, nearestPillNode,
+								game.getGhostLastMoveMade(g), DM.PATH));
+						stateSeer = StatesSeer.Away;
+
+						if (DEBUG)
+							GameView.addLines(game, Color.RED, ghostNode, nearestPillNode);
+					}
+					else {
+						nextMoveAgressive(game, ghostNode, pacmanNode, g, g.ordinal());
+					}
+				}
+				break;
+				
+			case RunningAway:
+				if (isGhostEdible(game, g) || pacmanCloseToPowerPill(game, pacmanNode, 25)) {
+					moves.put(g, game.getApproximateNextMoveAwayFromTarget(ghostNode, pacmanNode,
+							game.getGhostLastMoveMade(g), DM.PATH));
+				}
+				else {
+					int ghostDistance; // distance between the ghost and Pacman
+					ghostDistance = game.getShortestPathDistance(pacmanNode, ghostNode);
+					
+					// Go to next pill if it's not next to the Pacman
+					if (ghostDistance > awayFromPacmanDistance && game.getShortestPathDistance(ghostNode, nearestPillNode) > 1) {
+						moves.put(g, game.getApproximateNextMoveTowardsTarget(ghostNode, nearestPillNode,
+								game.getGhostLastMoveMade(g), DM.PATH));
+						stateSeer = StatesSeer.Away;
+
+						if (DEBUG)
+							GameView.addLines(game, Color.RED, ghostNode, nearestPillNode);
+					}
+					else // if the ghost is close enough, it chases Pacman
+						stateSeer = StatesSeer.Chasing;
+						nextMoveAgressive(game, ghostNode, pacmanNode, g, g.ordinal());
+				}
+				break;
 			}
 		}
-
-		return moves;
 	}
-	
-	private void SeerBehaviour() {
-		
-	}
-	
-	private void AntBehaviour() {
-		
-	}
-	
-	private void AggressiveBehaviour() {
-		
-	}
-	
-	private void AmbushBehaviour() {
-		
+	private void nextIntersectGhost(Game game) {
+		// We get Pacman current node
+		int pacmanNode = game.getPacmanCurrentNodeIndex();
 	}
 
 	private boolean pacmanCloseToPowerPill(Game game, int pcNode, int limit) {
