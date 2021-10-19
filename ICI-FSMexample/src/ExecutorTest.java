b853d24615393394e0093b0012d445cdd0fda342
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2021-10-19 18:08:23 +0200

    Actualizacion librerias, añadido ICI-FSMDemo

---
+++ ICI-FSMexample/src/ExecutorTest.java
@@ -0,0 +1,28 @@
+
+
+import pacman.Executor;
+import pacman.controllers.GhostController;
+import pacman.controllers.HumanController;
+import pacman.controllers.PacmanController;
+
+
+public class ExecutorTest {
+
+    public static void main(String[] args) {
+        Executor executor = new Executor.Builder()
+                .setTickLimit(4000)
+                .setGhostPO(false)
+                .setPacmanPO(false)
+                .setVisual(true)
+                .setScaleFactor(3.0)
+                .build();
+
+        PacmanController pacMan = new es.ucm.fdi.ici.practica2.demofsm.MsPacMan();
+        GhostController ghosts = new es.ucm.fdi.ici.practica2.demofsm.Ghosts();
+        
+        System.out.println( 
+        		executor.runGame(pacMan, ghosts, 40)
+        );
+        
+    }
+}
---
+++ ICI-FSMexample/src/es/ucm/fdi/ici/practica2/demofsm/mspacman/MsPacManInput.java
@@ -0,0 +1,19 @@
+package es.ucm.fdi.ici.practica2.demofsm.mspacman;
+
+import es.ucm.fdi.ici.Input;
+import pacman.game.Game;
+
+public class MsPacManInput extends Input {
+
+	public MsPacManInput(Game game) {
+		super(game);
+		
+	}
+
+	@Override
+	public void parseInput() {
+		// does nothing.
+
+	}
+
+}
