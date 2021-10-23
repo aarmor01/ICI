38456c4226952c812b1e1a8fa4487299a77705e9
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2021-10-23 22:52:45 +0200

    Update AnotherGhostOnRunAwayPath.java

--- MrsPacmanEngine/src/main/java/es/ucm/fdi/ici/c2122/practica2/grupo02/ghosts/transitions/AnotherGhostOnRunAwayPath.java
+++ MrsPacmanEngine/src/main/java/es/ucm/fdi/ici/c2122/practica2/grupo02/ghosts/transitions/AnotherGhostOnRunAwayPath.java
@@ -1,22 +1,27 @@
 package es.ucm.fdi.ici.c2122.practica2.grupo02.ghosts.transitions;
 
 import es.ucm.fdi.ici.Input;
+import es.ucm.fdi.ici.c2122.practica2.grupo02.ghosts.GhostInput;
 import es.ucm.fdi.ici.fsm.Transition;
+import pacman.game.Constants.GHOST;
 
 public class AnotherGhostOnRunAwayPath implements Transition {
-
-	public AnotherGhostOnRunAwayPath() {
+	GHOST ghost;
+	
+	public AnotherGhostOnRunAwayPath(GHOST ghost) {
 		super();
+		this.ghost = ghost;
 	}
 	
 	@Override
 	public boolean evaluate(Input in) {
+		GhostInput input = (GhostInput)in;
 		// TODO Auto-generated method stub
 		return false;
 	}
 	
 	@Override
 	public String toString() {
-		return ghost + " has been chasing MsPacMan for too long on the same path";
+		return ghost + " changes direction to not use the same path as another ghost";
 	}
 }
