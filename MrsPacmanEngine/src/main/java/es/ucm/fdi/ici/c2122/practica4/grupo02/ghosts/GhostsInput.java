fd04a166dc9afa247f8d53e0013a3ff100336327
aarmor01 <62306290+aarmor01@users.noreply.github.com>
2021-12-03 13:52:28 +0100

    Update GhostsInput.java

--- MrsPacmanEngine/src/main/java/es/ucm/fdi/ici/c2122/practica4/grupo02/ghosts/GhostsInput.java
+++ MrsPacmanEngine/src/main/java/es/ucm/fdi/ici/c2122/practica4/grupo02/ghosts/GhostsInput.java
@@ -11,26 +11,25 @@ import java.util.HashMap;
 public class GhostsInput extends FuzzyInput {
 
 	private double[] distance;
-	
-	
+
 	public GhostsInput(Game game) {
 		super(game);
 	}
-	
+
 	@Override
-	public void parseInput() {		
-		distance = new double[] {-1,-1,-1,-1};
-		
-		for(GHOST g: GHOST.values()) {
+	public void parseInput() {
+		distance = new double[] { -1, -1, -1, -1 };
+
+		for (GHOST g : GHOST.values()) {
 			int index = g.ordinal();
 			int pos = game.getGhostCurrentNodeIndex(g);
 			int pacManPos = game.getPacmanCurrentNodeIndex();
-			
-			if (pacManPos != -1) 
+
+			if (pacManPos != -1)
 				distance[index] = game.getDistance(pos, pacManPos, DM.PATH);
 		}
 	}
-	
+
 	public boolean isVisible(GHOST ghost) {
 		return distance[ghost.ordinal()] != -1;
 	}
@@ -38,11 +37,11 @@ public class GhostsInput extends FuzzyInput {
 	@Override
 	public HashMap<String, Double> getFuzzyValues() {
 		// puts the values on the fcl file input values
-		HashMap<String,Double> vars = new HashMap<String,Double>();
-		
-		for(GHOST g : GHOST.values()) 
-			vars.put(g.name()+"distanceToPacMan", distance[g.ordinal()]);
-		
+		HashMap<String, Double> vars = new HashMap<String, Double>();
+
+		for (GHOST g : GHOST.values())
+			vars.put(g.name() + "distanceToPacMan", distance[g.ordinal()]);
+
 		return vars;
 	}
 }
